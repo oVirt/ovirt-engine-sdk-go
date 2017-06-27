0593fc7ef6d52d4e1eee4d21f839d8b79a4516a4
imjoey <majunjiev@gmail.com>
2017-06-27 11:59:25 +0800

    replace StringInSlice method with more general Contains(a interface{}, list interface{}) method.

--- sdk/ovirtsdk4/utils.go
+++ sdk/ovirtsdk4/utils.go
@@ -1,10 +1,23 @@
 package ovirtsdk4
 
-func StringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
+import (
+	"reflect"
+)
+
+func Contains(obj interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
 			return true
 		}
 	}
+
 	return false
 }
