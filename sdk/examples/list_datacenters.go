794170c699a0f174e36d6abc6f50b5698c7f2e93
imjoey <majunjiev@gmail.com>
2017-08-01 10:49:11 +0800

    Add examples of get vms list and get datacenter list.

---
+++ sdk/examples/list_datacenters.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"../ovirtsdk4"
+)
+
+func main() {
+	inputRawURL := "https://10.1.111.229/ovirt-engine/api"
+
+	conn, err := ovirtsdk4.NewConnectionBuilder().
+		URL(inputRawURL).
+		Username("admin@internal").
+		Password("qwer1234").
+		Insecure(true).
+		Compress(true).
+		Timeout(time.Second * 10).
+		Build()
+	if err != nil {
+		fmt.Printf("Make connection failed, reason: %s\n", err.Error())
+	}
+	defer conn.Close()
+
+	// Get the reference to the "datacenters" service:
+	datacentersService := conn.SystemService().DataCentersService()
+
+	// Use the "list" method of the "datacenters" service to list all the datacenters of the system:
+	datacentersResponse, err := datacentersService.List().Send()
+	if err != nil {
+		fmt.Printf("Failed to get datacenter list, reason: %v\n", err)
+		return
+	}
+
+	// Print the datacenter names and identifiers:
+	for _, dc := range datacentersResponse.DataCenters() {
+		fmt.Printf("Datacenter - (name: %v, id: %v)\n", *dc.Name, *dc.Id)
+	}
+
+}
